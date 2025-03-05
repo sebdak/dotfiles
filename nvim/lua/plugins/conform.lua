376e0c0ab24c0e320032446f8cd446580d1fe49d
Sebastian Knudsen <sebastian.dahle.knudsen@vipps.no>
2025-03-05 15:19:36 +0100

    add toggling of formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -11,6 +11,21 @@ return {
       mode = "",
       desc = "Format buffer",
     },
+    {
+      '<leader>tf',
+      function()
+        -- If autoformat is currently disabled globally,
+        -- then enable it globally, otherwise disable it globally
+        if vim.g.disable_autoformat then
+          vim.cmd 'FormatEnable'
+          vim.notify 'Enabled autoformat globally'
+        else
+          vim.cmd 'FormatDisable'
+          vim.notify 'Disabled autoformat globally'
+        end
+      end,
+      desc = 'Toggle autoformat globally',
+    },
   },
   ---@module "conform"
   ---@type conform.setupOpts
@@ -32,11 +47,39 @@ return {
     default_format_opts = {
       lsp_format = "fallback",
     },
-    format_on_save = { timeout_ms = 1500 },
+    format_on_save = function()
+      if vim.g.disable_autoformat then
+        return
+      end
+      return { timeout_ms = 1500 }
+    end,
     formatters = {
       shfmt = {
         prepend_args = { "-i", "2" },
       },
     },
   },
+  config = function(_, opts)
+    require('conform').setup(opts)
+
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- :FormatDisable! disables autoformat for this buffer only
+        vim.b.disable_autoformat = true
+      else
+        -- :FormatDisable disables autoformat globally
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true, -- allows the ! variant
+    })
+
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
+  end,
 }
